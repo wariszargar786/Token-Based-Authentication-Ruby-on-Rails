9e5d49e71501bf8aeb269bc702b9012727842ff2
wariszargar786 <waris.zargar123@gmail.com>
2021-01-13 13:44:43 +0500

    Logout using ruby on rails

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::SessionsController < Devise::SessionsController
+  # for login
   before_action :sessions_params, only: :create
   before_action :load_user, only: :create
+
+  # for logout
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
+
   def create
     if @user.valid_password?(sessions_params[:password])
       json_response "Login Successful" , true, {user:@user}, :ok
@@ -9,12 +15,27 @@ class Api::V1::SessionsController < Devise::SessionsController
     end
   end
 
+  def destroy
+    sign_out @user
+    token = @user.generate_new_authentication_token
+    @user[:authentication_token] = token
+    @user.save
+    json_response "Logout Successfully ", true, {}, :ok
+  end
   private
   def sessions_params
     params.require(:signin).permit(:email, :password)
   end
   def load_user
     @user = User.find_for_database_authentication(email:sessions_params[:email])
+    if @user
+      return  @user
+    else
+      json_response "User not found", false , {}, :not_found
+    end
+  end
+  def valid_token
+    @user = User.where(authentication_token: request.headers["AUTH-TOKEN"]).first
     if @user
       return  @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       devise_scope :user do
         post 'register', to: 'registrations#create'
         post 'login', to: 'sessions#create'
+        delete 'logout', to: 'sessions#destroy'
       end
     end
   end
